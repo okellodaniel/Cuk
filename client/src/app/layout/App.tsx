91b28a6edf91d3298cebde30aa50766776a31aa2
Daniel Okello <43291086+okellodaniel@users.noreply.github.com>
2023-05-04 23:08:13 +0300

    Rearranged the folder structure

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,23 +1,29 @@
 import { useEffect, useState } from "react";
+import { Product } from "../models/Product";
 
 function App() {
   // Creating a react hook (Storing in State)
-  const [products, setProducts] = useState([
-    { name: 'Product 1', price: 100 },
-    { name: 'Product 2', price: 200 }
-  ])
+  const [products, setProducts] = useState<Product[]>([])
 
   useEffect(() => {
     fetch("http://localhost:5106/api/products")
       .then(response => response.json())
       .then(data => setProducts(data))
-  }, [])
+  })
 
   // function addProduct() {
   //   setProducts([...products, { name: 'Potats', price: 344 }])
   // }
   function addProduct() {
-    setProducts(prevState => [...prevState, { name: 'product ' + (prevState.length + 1), price: (prevState.length * 100) + 100 }]);
+    setProducts(prevState => [...prevState,
+    {
+      id: prevState.length + 1,
+      name: 'product ' + (prevState.length + 1),
+      price: (prevState.length * 100) + 100,
+      description: "Something",
+      pictureUrl: "https://picsum/photos/200",
+      brand: "Something"
+    }]);
   }
 
   return (
@@ -25,7 +31,7 @@ function App() {
       <h1>Cuk App</h1>
       <ul>
         {products.map((product, index) => {
-          return <li key={index}> {product.name} - {product.price}</li>
+          return <li key={index}> {product.name} - {product.price} - {product.pictureUrl}</li>
         })}
       </ul>
       <button onClick={addProduct}>Add Value</button>
