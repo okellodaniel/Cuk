3b2cf47d8641a7d95bb8f85a157ce5c7d6b3a5ef
Daniel Okello <43291086+okellodaniel@users.noreply.github.com>
2023-05-06 23:51:06 +0300

    Added a sample compenent

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { Product } from "../models/Product";
+import Catalogue from "../../features/catalogue/catalogue";
+import { Typography } from "@mui/material";
 
 function App() {
   // Creating a react hook (Storing in State)
@@ -9,7 +11,7 @@ function App() {
     fetch("http://localhost:5106/api/products")
       .then(response => response.json())
       .then(data => setProducts(data))
-  })
+  }, [])
 
   // function addProduct() {
   //   setProducts([...products, { name: 'Potats', price: 344 }])
@@ -21,21 +23,16 @@ function App() {
       name: 'product ' + (prevState.length + 1),
       price: (prevState.length * 100) + 100,
       description: "Something",
-      pictureUrl: "https://picsum/photos/200",
+      pictureUrl: "https://picsum.photos/200",
       brand: "Something"
     }]);
   }
 
   return (
-    <div>
-      <h1>Cuk App</h1>
-      <ul>
-        {products.map((product, index) => {
-          return <li key={index}> {product.name} - {product.price} - {product.pictureUrl}</li>
-        })}
-      </ul>
-      <button onClick={addProduct}>Add Value</button>
-    </div>
+    <>
+      <Typography variant="h1">Cuk App</Typography>
+      <Catalogue products={products} addProduct={addProduct} />
+    </>
 
   );
 }
--- client/src/features/catalogue/catalogue.tsx
+++ client/src/features/catalogue/catalogue.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import { Avatar, ListItem, ListItemAvatar, ListItemText, List, ListItemButton, Button } from "@mui/material";
+import { Product } from "../../app/models/Product"
 
-function catalogue() {
-  return (
-    <>
-        <h1></h1>
-    </>
-  )
+interface props {
+    products: Product[];
+    addProduct: () => void;
+
 }
-
-export default catalogue
+export default function Catalogue({ products, addProduct }: props) {
+    return (
+        <>
+            <List>
+                {products.map(product => {
+                    return (
+                        <ListItem>
+                            <ListItemAvatar>
+                                <Avatar src={product.pictureUrl} />
+                            </ListItemAvatar>
+                            <ListItemText>
+                                {product.name} - {product.price}
+                            </ListItemText>
+                        </ListItem>
+                    )
+                })}
+                {/* {products.map(product => {
+                    return <li key={product.id}> {product.name} - {product.price} - {product.pictureUrl}</li>
+                })} */}
+            </List>
+            <Button onClick={addProduct}>Add Value</Button>
+        </>
+    )
+}
